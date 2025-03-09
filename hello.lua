8f51a4a82adb1236e5ed88a675500ee238182c97
Les Matheson <les.matheson@gmail.com>
2025-03-09 13:50:45 -0400

    reaper finally stopped complaining about bad paths!

--- hello.lua
+++ hello.lua
@@ -1,5 +1,8 @@
 
-package.cpath = package.cpath .. ";" .. reaper.GetResourcePath() .. '/Scripts/Daniel Lumertz Scripts/LUA Sockets/socket module/socket/?.dll'
+-- package.cpath = package.cpath .. ";" .. reaper.GetResourcePath() .. '/Scripts/lua-sockets/socket-module/?.lua'
+package.path = package.path .. ";" .. reaper.GetResourcePath() .. '/Scripts/?.lua'
+package.path = package.path .. ";" .. reaper.GetResourcePath() .. '/Scripts/lua-sockets/socket-module/?.lua'
+package.cpath = package.cpath .. ";" .. reaper.GetResourcePath() .. '/Scripts/lua-sockets/socket-module/socket/core.dll'
 
 require("mobdebug").start()
 
@@ -7,5 +10,4 @@ local track = reaper.GetTrack(0, 0)
 reaper.ShowConsoleMsg('Hello world')
 local t = {track, track, 10, 20, 30, {50,60,70,{80,90,100}}}
 reaper.ShowConsoleMsg('Finishing script')
-
 
